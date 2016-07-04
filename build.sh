2b4d55b3b4caa1a22d1f9a8aa90923a7a37e3154
Woodrow Shen <woodrow.shen@gmail.com>
2016-07-04 17:22:40 +0800

    Use ubuntu-core revision 122 as default. And workaround to enable all pre-installed snap services

--- build.sh
+++ build.sh
@@ -2,7 +2,23 @@ set -x
 
 #https://people.canonical.com/~mvo/all-snaps/ubuntu-device-flash
 
-sudo ./ubuntu-device-flash core 16 -o havasu.img --gadget=canonical-pc --kernel=canonical-pc-linux --os ubuntu-core --size 4 \
---enable-ssh --developer-mode --channel=edge
+TODAY=$(date +%Y%m%d)
+MINOR_RELEASE=$1
+PROJECT=$2
+CHANNEL=$3
 
+if [ -z $CHANNEL ] ; then
+	CHANNEL=edge
+fi
 
+./ubuntu-device-flash --verbose core 16 \
+	--channel $CHANNEL \
+	--size 4 \
+	--enable-ssh \
+	--gadget canonical-pc \
+	--kernel canonical-pc-linux \
+	--os ubuntu-core-122.snap \
+	--install webdm \
+	--install bluez \
+	--install network-manager \
+	-o $PROJECT-$TODAY-$MINOR_RELEASE.img
