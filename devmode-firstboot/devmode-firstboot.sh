32504fbaeba2611982ed30bc19846c02864a36d8
Woodrow Shen <woodrow.shen@gmail.com>
2016-08-17 17:29:44 +0800

    Update UC 16 for fixing correct interfaces

--- devmode-firstboot/devmode-firstboot.sh
+++ devmode-firstboot/devmode-firstboot.sh
@@ -4,13 +4,17 @@ set -x
 
 cd /writable/system-data/var/lib/devmode-firstboot
 
-for DEVMODESNAPS in *.snap ; do
-	snap install --devmode $DEVMODESNAPS
-done
+#for DEVMODESNAPS in *.snap ; do
+#	snap install --devmode $DEVMODESNAPS
+#done
+
+snap install tpm_1.2-1_amd64.snap
+snap install --devmode uefi-fw-tools_1.0-0.7.2+git_amd64.snap
 
 snap connect modem-manager:mmcli modem-manager:service
-snap connect network-manager:service modem-manager:service
 snap connect network-manager:nmcli network-manager:service
+snap connect network-manager:modem-manager modem-manager:service
+snap connect network-manager:ppp ubuntu-core:ppp
 snap connect bluez:client bluez:service
 snap connect tpm:tpm ubuntu-core:tpm
 
@@ -24,4 +28,4 @@ systemctl restart snap.bluez.obex
 sed -i 's/After=snapd.frameworks.target/After=network-online.target snapd.frameworks.target/g' /etc/systemd/system/snap.snapweb.snapweb.service
 sed -i 's/Requires=snapd.frameworks.target/Requires=network-online.target snapd.frameworks.target/g' /etc/systemd/system/snap.snapweb.snapweb.service
 
-#reboot
+reboot
